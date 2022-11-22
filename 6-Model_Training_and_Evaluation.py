523aa2780a043bd0efa3b22cdc4553684b72dc6b
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2022-11-22 16:06:11 +0800

    Modify Model Training for Ml_Model_Training.py Code

--- 6-Model_Training_and_Evaluation.py
+++ 6-Model_Training_and_Evaluation.py
@@ -9,76 +9,85 @@ import tqdm.contrib.itertools
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, Normalizer 
 
-import ML_two_class_for_autogluon
+import ML_two_class
 
-# 輸入原始資料
-with gzip.GzipFile("preprocess_data/Feature_Engineer_Data-Flow-2.gzip", "rb") as f:
-    raw_data = pickle.load(f)
-ID_split = pd.read_pickle("preproceseed_data/ID_split_MergeData-4.gzip", "gzip")
 
 
 # 定義許多變數
 PK = "Patient"
-Time_column = "Time"
+Time_column = "Predict_Time"
 target = "IDH" 
 standardization_list = [None, "standardization", "normalization", "min-max_scaler"]
 decomposition_list = [None] 
-split_data_stratify_list = reversed([None, PK, target, f"{PK}_{target}"])
-inputFeatures = [i for i in raw_data.columns if i not in [PK, Time_column, target]]
+feature_selection_method_list = [None, "SFS", "SBS", "SFFS", "SFBS", "RFECV"]
+metaData_list = ["MetaData-1", "MetaData-2", "MetaData-3"] * 2
+mergeData_list = [*["Merge-1-patient"]*3, *["Merge-1-IDH"]*3]
 
 ML_totalResult, FI_totalResult = list(), list()
-
-for standardization_method, decomposition_method, data_id in itertools.product(standardization_list, decomposition_list, range(1, 31, 1)): 
-    trainData = raw_data.loc[ID_split.query("data_id == @data_id and split == @train")["ID_split"].iloc[0], inputFeatures + [target]]
-    valiData = raw_data.loc[ID_split.query("data_id == @data_id and split == @vali")["ID_split"].iloc[0], inputFeatures + [target]]
-    testData = raw_data.loc[ID_split.query("data_id == @data_id and split == @test")["ID_split"].iloc[0], inputFeatures + [target]] 
-
-    if standardization_method == "standardization":
-        ### Standardization ###
-        standardization = StandardScaler().fit(trainData[inputFeatures])
-        trainData = pd.concat([
-            pd.DataFrame(standardization.transform(trainData[inputFeatures]), columns = inputFeatures), trainData[target]
-        ], axis = 1)
-        valiData = pd.concat([
-            pd.DataFrame(standardization.transform(valiData[inputFeatures]), columns = inputFeatures), valiData[target]
-        ], axis = 1)
-        testData = pd.concat([
-            pd.DataFrame(standardization.transform(testData[inputFeatures]), columns = inputFeatures), testData[target]
-        ], axis = 1)
-        ### Standardization ###
-    elif standardization_method == "normalization":
-        ### Normalization ###
-        normalization = Normalizer().fit(trainData[inputFeatures])
-        trainData = pd.concat([
-            pd.DataFrame(normalization.transform(trainData[inputFeatures]), columns = inputFeatures), trainData[target]
-        ], axis = 1)
-        valiData = pd.concat([
-            pd.DataFrame(normalization.transform(valiData[inputFeatures]), columns = inputFeatures), valiData[target]
-        ], axis = 1)
-        testData = pd.concat([
-            pd.DataFrame(normalization.transform(testData[inputFeatures]), columns = inputFeatures), testData[target]
-        ], axis = 1)
-        ### Normalization ###            
+for one_metaData, one_mergeData in zip(metaData_list, mergeData_list):
     
-    # 模型訓練
-    totalResult = ML_two_class_for_autogluon.model_fit(
-        data_id = data_id,
-        trainData = trainData,
-        valiData = valiData,
-        testData = testData,
-        input_features = inputFeatures,
-        target_label = target
-    )    
-    basic_result = {
-        "Data_ID": data_id,
-        "Split_Data_Stratify": one_split_data_stratify,
-        "Standardization": standardization_method,
-        "Decomposition": decomposition_method 
-    }
-    ML_totalResult.extend([{**basic_result, **i} for i in totalResult[0]])
-    FI_totalResult.extend([{**basic_result, **i} for i in totalResult[1]])
+    # 輸入原始資料
+    raw_data = pd.read_pickle(os.path.join("preprocessed_data", f"Feature_Engineer_{one_metaData}.gzip"), "gzip")
+    ID_split = pd.read_pickle(os.path.join("preprocessed_data", f"ID_split_{one_mergeData}.gzip"), "gzip")
+    inputFeatures = [i for i in raw_data.columns if i not in [PK, Time_column, target]]
+    
+    for standardization_method, decomposition_method, feature_selection_method, data_id in itertools.product(standardization_list[:1], 
+                                                                                                             decomposition_list, 
+                                                                                                             feature_selection_method_list,
+                                                                                                             range(1, 2, 1)): 
+        trainData = raw_data.loc[ID_split.query("data_id == @data_id and split == 'train'")["id_list"].iloc[0], [*inputFeatures, target]]
+        valiData = raw_data.loc[ID_split.query("data_id == @data_id and split == 'vali'")["id_list"].iloc[0], [*inputFeatures, target]]
+        testData = raw_data.loc[ID_split.query("data_id == @data_id and split == 'test'")["id_list"].iloc[0], [*inputFeatures, target]] 
 
-writer = pd.ExcelWriter("result/ML-result-Flow-6_and_10_and_14_and_18_and_22.xlsx")
+        if standardization_method == "standardization":
+            ### Standardization ###
+            standardization = StandardScaler().fit(trainData[inputFeatures])
+            trainData = pd.concat([
+                pd.DataFrame(standardization.transform(trainData[inputFeatures]), columns = inputFeatures), trainData[target]
+            ], axis = 1)
+            valiData = pd.concat([
+                pd.DataFrame(standardization.transform(valiData[inputFeatures]), columns = inputFeatures), valiData[target]
+            ], axis = 1)
+            testData = pd.concat([
+                pd.DataFrame(standardization.transform(testData[inputFeatures]), columns = inputFeatures), testData[target]
+            ], axis = 1)
+            ### Standardization ###
+        elif standardization_method == "normalization":
+            ### Normalization ###
+            normalization = Normalizer().fit(trainData[inputFeatures])
+            trainData = pd.concat([
+                pd.DataFrame(normalization.transform(trainData[inputFeatures]), columns = inputFeatures), trainData[target]
+            ], axis = 1)
+            valiData = pd.concat([
+                pd.DataFrame(normalization.transform(valiData[inputFeatures]), columns = inputFeatures), valiData[target]
+            ], axis = 1)
+            testData = pd.concat([
+                pd.DataFrame(normalization.transform(testData[inputFeatures]), columns = inputFeatures), testData[target]
+            ], axis = 1)
+            ### Normalization ###            
+        
+        # 模型訓練
+        
+        totalResult = ML_two_class.model_fit(data_id = 1,
+                                            trainData = trainData,
+                                            valiData = valiData,
+                                            testData = testData,
+                                            input_features = inputFeatures,
+                                            target_label = target,
+                                            target_type = "classification",
+                                            main_metric = "f1",
+                                            feature_selection_method = feature_selection_method)
+        basic_result = {
+            "Data_ID": data_id,
+            "MetaData_ID": one_metaData,
+            "Standardization": standardization_method,
+            "Decomposition": decomposition_method,
+            "FeatureSelection": feature_selection_method,
+        }
+        ML_totalResult.extend([{**basic_result, **i} for i in totalResult[0]])
+        FI_totalResult.extend([{**basic_result, **i} for i in totalResult[1]])
+
+writer = pd.ExcelWriter("result/ML-result-test.xlsx")
 pd.DataFrame(ML_totalResult).to_excel(writer, index = None, sheet_name = "Model_Evaluation")
 pd.DataFrame(FI_totalResult).to_excel(writer, index = None, sheet_name = "Permutation Importance")
 writer.close() 
