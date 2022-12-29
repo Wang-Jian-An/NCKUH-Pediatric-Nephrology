c963ccfed3b1aeb0a3766fff0aae1a0803426b96
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2022-12-29 10:40:13 +0800

    feat: Store Model

--- ML_Model_Training.py
+++ ML_Model_Training.py
@@ -1,3 +1,6 @@
+import gzip
+import os
+import pickle
 import numpy as np
 import pandas as pd
 
@@ -20,32 +23,6 @@ trainData, valiData, inputFeatures, target, main_metrics, target_type
 
 """
 
-model_dict = {
-    "classification": {
-        "Random Forest with Entropy": RandomForestClassifier(criterion = "entropy"),
-        "Random Forest with Gini": RandomForestClassifier(criterion = "gini"),
-        "Extra Tree with Entropy": ExtraTreeClassifier(criterion = "entropy"),
-        "Extra Tree with Gini": ExtraTreeClassifier(criterion = "gini"),
-        "XGBoost": XGBClassifier(),
-        "NGBoost": NGBClassifier(),
-        "CatBoost": CatBoostClassifier(),
-        "LightGBM": LGBMClassifier(),
-        "NeuralNetwork": MLPClassifier()
-    },
-    "regression": {
-        "Random Forest with Entropy": RandomForestRegressor(criterion = "entropy"),
-        "Random Forest with Gini": RandomForestRegressor(criterion = "gini"),
-        "Extra Tree with Entropy": ExtraTreeRegressor(criterion = "entropy"),
-        "Extra Tree with Gini": ExtraTreeRegressor(criterion = "gini"),
-        "XGBoost": XGBRegressor(),
-        "NGBoost": NGBRegressor(),
-        "CatBoost": CatBoostRegressor(),
-        "LightGBM": LGBMRegressor(),
-        "NeuralNetwork": MLPRegressor()
-    }
-}
-
-
 class model_training_and_hyperparameter_tuning():
     def __init__(self, 
                  trainData: pd.DataFrame, 
@@ -55,12 +32,12 @@ class model_training_and_hyperparameter_tuning():
                  target_type, 
                  model_name, 
                  feature_selection_method, 
-                 main_metric):
+                 main_metric,
+                 model_file_name = None):
         
         """
         feature_selection_method: SBS、SFS、SFBS、SFFS、RFECV
         """
-        
         self.trainData = trainData
         self.valiData = valiData
         self.trainData_valiData = pd.concat([
@@ -70,70 +47,89 @@ class model_training_and_hyperparameter_tuning():
         self.target = target
         self.target_type = target_type
         self.model_name = model_name
+        self.n_trials = 300 if "Extra Tree" in self.model_name else 100
         self.main_metric = main_metric
         self.feature_selection_method = feature_selection_method
+        self.model_file_name = model_file_name
         return 
+    
     def model_training(self):
         
         if self.feature_selection_method is not None:
             self.feature_selection()
         
+        ### Use Optuna to tune hyperparameter ###
         study = optuna.create_study(direction = "minimize")
-        optuna.logging.set_verbosity(optuna.logging.WARNING)
-        study.optimize(self.objective_function, n_trials = 10)
+        study.optimize(self.objective_function, n_trials = self.n_trials, n_jobs = -1)
+        ### Use Optuna to tune hyperparameter ###
+        
+        ### Output the result of hyperparameter tuning ###
+        study_trial_data = study.trials_dataframe()
+        study_trial_data["Model"] = self.model_name
 
-        model = model_dict[self.target_type][self.model_name]
+        try:
+            fig = optuna.visualization.plot_param_importances(study)
+        except: 
+            fig = None
+        ### Output the result of hyperparameter tuning ###
+
+        model = self.choose_one_model()
         model.set_params(**study.best_params)
 
         model.fit(self.trainData_valiData[self.inputFeatures], self.trainData_valiData[self.target])
+        if self.model_file_name:
+            with gzip.GzipFile(os.path.join("result", self.model_file_name), "wb") as f:
+                pickle.dump(model, f)
         return {
-            "Features": self.inputFeatures,
-            "Model": model
+            "Features": self.inputFeatures, 
+            "Model": model,
+            "Hyperparameter_Tuning": study_trial_data,
+            "Param_Importance": fig
             }
 
     def feature_selection(self):
         if self.feature_selection_method == "SFS":
-            featureSelectionObj = SequentialFeatureSelector(estimator = model_dict[self.target_type][self.model_name],
+            featureSelectionObj = SequentialFeatureSelector(estimator = self.choose_one_model(),
                                                             k_features = int(round(len(self.inputFeatures) / 2, 0)),
                                                             forward = True,
                                                             floating = False,
                                                             scoring = self.main_metric,
                                                             verbose = 1,
-                                                            n_jobs = -1,
+                                                            n_jobs = 6,
                                                             cv = 5).fit(X = self.trainData_valiData[self.inputFeatures], y = self.trainData_valiData[self.target])
             
         elif self.feature_selection_method == "SBS":
-            featureSelectionObj = SequentialFeatureSelector(estimator = model_dict[self.target_type][self.model_name],
+            featureSelectionObj = SequentialFeatureSelector(estimator = self.choose_one_model(),
                                                             k_features = int(round(len(self.inputFeatures) / 2, 0)),
                                                             forward = False,
                                                             floating = False,
                                                             scoring = self.main_metric,
                                                             verbose = 1,
-                                                            n_jobs = -1,
+                                                            n_jobs = 6,
                                                             cv = 5).fit(X = self.trainData_valiData[self.inputFeatures], y = self.trainData_valiData[self.target])
         elif self.feature_selection_method == "SFFS":
-            featureSelectionObj = SequentialFeatureSelector(estimator = model_dict[self.target_type][self.model_name],
+            featureSelectionObj = SequentialFeatureSelector(estimator = self.choose_one_model(),
                                                             k_features = int(round(len(self.inputFeatures) / 2, 0)),
                                                             forward = True,
                                                             floating = True,
                                                             scoring = self.main_metric,
                                                             verbose = 1,
-                                                            n_jobs = -1,
+                                                            n_jobs = 6,
                                                             cv = 5).fit(X = self.trainData_valiData[self.inputFeatures], y = self.trainData_valiData[self.target])
         elif self.feature_selection_method == "SFBS":
-            featureSelectionObj = SequentialFeatureSelector(estimator = model_dict[self.target_type][self.model_name],
+            featureSelectionObj = SequentialFeatureSelector(estimator = self.choose_one_model(),
                                                             k_features = int(round(len(self.inputFeatures) / 2, 0)),
                                                             forward = True,
                                                             floating = True,
                                                             scoring = self.main_metric,
                                                             verbose = 1,
-                                                            n_jobs = -1,
+                                                            n_jobs = 6,
                                                             cv = 5).fit(X = self.trainData_valiData[self.inputFeatures], y = self.trainData_valiData[self.target])
         elif self.feature_selection_method == "RFECV":
-            featureSelectionObj = RFECV(estimator = model_dict[self.target_type][self.model_name],
+            featureSelectionObj = RFECV(estimator = self.choose_one_model(),
                                         min_features_to_select = int(round(len(self.inputFeatures) / 2, 0)),
                                         verbose = 1,
-                                        n_jobs = -1,
+                                        n_jobs = 6,
                                         cv = 5).fit(X = self.trainData_valiData[self.inputFeatures], y = self.trainData_valiData[self.target])
         
         if self.feature_selection_method == "RFECV":
@@ -141,21 +137,49 @@ class model_training_and_hyperparameter_tuning():
         else:
             self.inputFeatures = list(featureSelectionObj.k_feature_names_)
 
+    def choose_one_model(self):
+
+        if self.target_type == "classification":
+            if self.model_name == "Random Forest with Entropy":
+                model = RandomForestClassifier(**{"criterion": "entropy"})
+            elif self.model_name == "Random Forest with Gini":
+                model = RandomForestClassifier(**{"criterion": "gini"})
+            elif self.model_name == "Extra Tree with Entropy":
+                model = ExtraTreeClassifier(**{"criterion": "entropy"})
+            elif self.model_name == "Extra Tree with Gini":
+                model = ExtraTreeClassifier(**{"criterion": "gini"})
+            elif self.model_name == "XGBoost":
+                model = XGBClassifier()
+            elif self.model_name == "NGBoost":
+                model = XGBClassifier()
+            elif self.model_name == "CatBoost":
+                model = CatBoostClassifier()
+            elif self.model_name == "LightGBM":
+                model = LGBMClassifier()
+            elif self.model_name == "NeuralNetwork":
+                pass
+            pass
+        elif self.target_type == "regression":
+            pass
+        return model
+
     def objective_function(self, trial):
 
-        model_parameter_name = {
-            "Random Forest with Entropy": "Random Forest",
-            "Random Forest with Gini": "Random Forest",
-            "Extra Tree with Entropy": "Extra Tree",
-            "Extra Tree with Gini": "Extra Tree",
-            "XGBoost": "XGBoost",
-            "NGBoost": "NGBoost",
-            "CatBoost": "CatBoost",
-            "LightGBM": "LightGBM",
-            "NeuralNetwork": "NeuralNetwork"
-        }[self.model_name]
-        model = model_dict[self.target_type][self.model_name]
-        model.set_params(**self.model_parameter_for_optuna(trial, model_name = model_parameter_name))
+        # model_parameter_name = {
+        #     "Random Forest with Entropy": "Random Forest",
+        #     "Random Forest with Gini": "Random Forest",
+        #     "Extra Tree with Entropy": "Extra Tree",
+        #     "Extra Tree with Gini": "Extra Tree",
+        #     "XGBoost": "XGBoost",
+        #     "NGBoost": "NGBoost",
+        #     "CatBoost": "CatBoost",
+        #     "LightGBM": "LightGBM",
+        #     "NeuralNetwork": "NeuralNetwork"
+        # }[self.model_name]
+        # model = model_dict[self.target_type][self.model_name]
+
+        model = self.choose_one_model()
+        model.set_params(**self.model_parameter_for_optuna(trial))
         model.fit(self.trainData[self.inputFeatures], self.trainData[self.target])
         
         if self.main_metric == "accuracy":
@@ -166,10 +190,10 @@ class model_training_and_hyperparameter_tuning():
             metric = roc_auc_score(y_true = self.valiData[self.target], y_pred = model.predict(self.valiData[self.inputFeatures]))
         return -metric
 
-    def model_parameter_for_optuna(self, trial, model_name):
-        if model_name == "Random Forest": 
+    def model_parameter_for_optuna(self, trial):
+        if self.model_name == "Random Forest with Entropy" or self.model_name == "Random Forest with Gini": 
             return {
-            "n_estimators": trial.suggest_int("n_estimators", 50, 500),
+            "n_estimators": trial.suggest_int("n_estimators", 2, 1000),
             "max_depth": trial.suggest_int("max_depth", 5, 50),
             "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
             "min_samples_leaf": trial.suggest_int("min_samples_leaf", 2, 100),
@@ -180,20 +204,21 @@ class model_training_and_hyperparameter_tuning():
             "oob_score": trial.suggest_categorical("oob_score", [False, True]),
             "ccp_alpha": trial.suggest_float("ccp_alpha", 0.0, 1.0),
         }
-        elif model_name == "Extra Tree":
+        elif self.model_name == "Extra Tree with Entropy" or self.model_name == "Extra Tree with Gini":
             return {
-            "max_depth": trial.suggest_int("max_depth", 5, 50),
-            "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
-            "min_samples_leaf": trial.suggest_int("min_samples_leaf", 2, 100),
+            "splitter": trial.suggest_categorical("splitter", ["random", "best"]),
+            "max_depth": trial.suggest_int("max_depth", 2, 100),
+            "min_samples_split": trial.suggest_float("min_samples_split", 0.01, 0.9),
+            "min_samples_leaf": trial.suggest_float("min_samples_leaf", 0.01, 0.9),
             "min_weight_fraction_leaf": trial.suggest_float("min_weight_fraction_leaf", 0.0, 0.5),
             "max_features": trial.suggest_categorical("max_features", ["sqrt", "log2"]),
-            "max_leaf_nodes": trial.suggest_int("max_leaf_nodes", 2, 100),
+            "max_leaf_nodes": trial.suggest_int("max_leaf_nodes", 2, 200),
             "min_impurity_decrease": trial.suggest_float("min_impurity_decrease", 0.0, 0.9),
             "ccp_alpha": trial.suggest_float("ccp_alpha", 0.0, 1.0),
         }
-        elif model_name == "XGBoost":
+        elif self.model_name == "XGBoost":
             return {
-            "n_estimators": trial.suggest_int("n_estimators", 50, 500),
+            "n_estimators": trial.suggest_int("n_estimators", 2, 1000),
             "max_depth": trial.suggest_int("max_depth", 5, 50), 
             "max_leaves": trial.suggest_int("max_leaves", 2, 30), 
             "max_bin": trial.suggest_int("max_bin", 2, 10), 
@@ -206,7 +231,7 @@ class model_training_and_hyperparameter_tuning():
             "reg_alpha": trial.suggest_float("reg_alpha", 0.1, 0.9),
             "reg_lambda": trial.suggest_float("reg_lambda", 0.1, 0.9)
         }
-        elif model_name == "NGBoost": 
+        elif self.model_name == "NGBoost": 
             return {
             "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
             "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-1),
@@ -214,7 +239,7 @@ class model_training_and_hyperparameter_tuning():
             # "col_sample": trial.suggest_float("col_sample", 0.1, 0.5),
             # "tol": trial.suggest_float("tol", 1e-6, 1e-2),
         }
-        elif model_name == "CatBoost":
+        elif self.model_name == "CatBoost":
             return {
             "iterations": trial.suggest_int("iterations", 100, 1000), # 樹的數量
             "od_type": trial.suggest_categorical("od_type", ["IncToDec", "Iter"]), # 過擬合偵測器
@@ -226,7 +251,7 @@ class model_training_and_hyperparameter_tuning():
             "border_count": trial.suggest_int("border_count", 128, 512),
             "grow_policy": trial.suggest_categorical("grow_policy", ["SymmetricTree", "Depthwise", "Lossguide"]), # 定義如何建構 greedy tree
         }
-        elif model_name == "LightGBM": 
+        elif self.model_name == "LightGBM": 
             return {
             # "boosting_type": trial.suggest_categorical("boosting_type", ['gbdt', "rf"]),
             "num_leaves": trial.suggest_int("num_leaves", 2, 100),
@@ -241,25 +266,17 @@ class model_training_and_hyperparameter_tuning():
             "reg_alpha": trial.suggest_float("reg_alpha", 0.01, 0.9),
             "reg_lambda": trial.suggest_float("reg_lambda", 0.01, 0.9),
         }
-        elif model_name == "NeuralNetwork":
+        elif self.model_name == "NeuralNetwork":
+            hidden_layer_sizes_trial = [k for i in range(1, 8) for k in [j for j in itertools.combinations([50, 100, 150, 200, 150, 100, 50], i)]]
             return {
-            "hidden_layer_sizes": (100,),
-            "activation": "relu",
-            "alpha": 0.0001,
-            "learning_rate": "constant",
-            "learning_rate_init": 0.001,
-            "power_t": 0.5,
-            "max_iter": 200,
-            "tol": 1e-4,
-            "warm_start": False,
-            "momentum": 0.9,
-            "nesterovs_momentum": True,
-            "early_stopping": False,
-            "validation_fraction": 0.1,
-            "beta_1": 0.9,
-            "beta_2": 0.999,
-            "epsilon": 1e-8,
-            "n_iter_no_change": 10,
-            "max_fun": 15000
+            "hidden_layer_sizes": trial.suggest_categorical("hidden_layer_sizes", hidden_layer_sizes_trial), 
+            "activation": "relu", 
+            "alpha": trial.suggest_float("alpha", 1e-5, 1e-3), 
+            "learning_rate": "adaptive", 
+            "learning_rate_init": trial.suggest_float("learning_rate_init", 1e-5, 1e-1), 
+            "max_iter": 500, 
+            "tol": 1e-4, 
+            "momentum": trial.suggest_float("momentum", 0.01, 0.9), 
+            "early_stopping": True, 
+            "n_iter_no_change": 10, 
         }
-    
